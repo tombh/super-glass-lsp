935f129242751cad454250e7a72bd6907e81bd79
Thomas Buckley-Houston <tom@tombh.co.uk>
2022-09-27 17:17:48 -0300

    feat: Specify ANY CLI tool with Py format string
    Currently suports `jq`

--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 
 Parses the output of linters, formatters, style checkers, etc and converts them to LSP.
 
-Conforms to the config format of https://github.com/mattn/efm-langserver#example-for-configyaml
+Inspired by: https://github.com/mattn/efm-langserver#example-for-configyaml
 
-Probably only supports LSP diagnostics and formatting.
+Probably will only ever support LSP diagnostics and formatting.
 
 ### TODO
-* [ ] Convert EFM's config tokens to Regex
 * [ ] Configs for various editors
 * [ ] Docs
 * [ ] Use public version of Pygls
--- src/lsp/definition.py
+++ src/lsp/definition.py
@@ -1,3 +1,9 @@
+import logging
+import subprocess
+from typing import List
+
+# type: ignore
+from parse import parse
 from pygls.lsp.methods import TEXT_DOCUMENT_DID_CHANGE, TEXT_DOCUMENT_DID_OPEN
 from pygls.lsp.types import (Diagnostic, DidChangeTextDocumentParams,
                              DidOpenTextDocumentParams, Position, Range)
@@ -11,30 +17,72 @@ class CLIToolsLanguageServer(LanguageServer):
 
 server = CLIToolsLanguageServer()
 
+config = {
+    "command": ["jq", "."],
+    "efm": "{msg} at line {line:d}, column {col:d}",
+    "language_id": "json",
+}
+
 
 @server.feature(TEXT_DOCUMENT_DID_CHANGE)
 async def did_change(params: DidChangeTextDocumentParams):
-    diagnose(document_uri=params.text_document.uri)
+    diagnose(
+        params.text_document.uri,
+    )
 
 
 @server.feature(TEXT_DOCUMENT_DID_OPEN)
 async def did_open(params: DidOpenTextDocumentParams):
-    diagnose(document_uri=params.text_document.uri)
-
-
-def diagnose(document_uri: str):
-    line = 0
-    col = 0
-    message = "Hello LSP World!"
-
-    text_doc = server.workspace.get_document(document_uri)
-
-    diagnostic = Diagnostic(
-        range=Range(
-            start=Position(line=line, character=col),
-            end=Position(line=line, character=col + 2),
-        ),
-        message=message,
-        source=type(server).__name__,
+    diagnose(
+        params.text_document.uri,
     )
-    server.publish_diagnostics(text_doc.uri, [diagnostic])
+
+
+def diagnose(uri: str):
+    document = server.workspace.get_document(uri)
+
+    if document.language_id != config["language_id"]:
+        return
+
+    result = subprocess.run(
+        config["command"],
+        input=document.source,
+        text=True,
+        capture_output=True,
+        check=False,
+    )
+    output = result.stderr.strip()
+
+    diagnostics: List[Diagnostic] = []
+
+    if not output:
+        server.publish_diagnostics(document.uri, diagnostics)
+        return
+
+    parsed = parse(config["efm"], output)
+    if parsed is None:
+        # TODO: What's the proper way of communicating this?
+        diagnostic = Diagnostic(
+            range=Range(
+                start=Position(line=0, character=0),
+                end=Position(line=0, character=1),
+            ),
+            message="CLI Tool LSP failed to parse CLI output",
+            source=type(server).__name__,
+        )
+    else:
+        line = parsed["line"]
+        col = parsed["col"]
+        message = parsed["msg"]
+
+        diagnostic = Diagnostic(
+            range=Range(
+                start=Position(line=line, character=col),
+                end=Position(line=line, character=col),
+            ),
+            message=message,
+            source=type(server).__name__,
+        )
+    diagnostics.append(diagnostic)
+
+    server.publish_diagnostics(document.uri, diagnostics)
