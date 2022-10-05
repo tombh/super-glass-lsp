8620db5e58d18ed22fd83f399e3bbf3c9f336f20
Thomas Buckley-Houston <tom@tombh.co.uk>
2022-10-04 23:13:42 -0300

    feat: Client-provided configs. Multiline parsing

---
+++ src/lsp/clitool_config.py
@@ -0,0 +1,53 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class OutputParsingConfig(BaseModel):
+    """Config for the output of CLI tools"""
+
+    format: str = Field(None)
+    """
+    Token definitions for parsing the output of the CLI tool.
+    This a Python token string that defines the components of a parsable line.
+    `{msg}`, `{line:d}` and `{col:d}` are required.
+    Eg; "{msg} at line {line:d}, column {col:d}"
+
+    See: https://docs.python.org/3/library/string.html#format-specification-mini-language  # nopep8
+    """
+
+    line_offset: Optional[int] = Field(0)
+    """The line index offset if the CLI tool doesn't start at 0, for example"""
+
+    col_offset: Optional[int] = Field(0)
+    """The col index offset if the CLI tool doesn't start at 0, for example"""
+
+
+# TODO: Maybe this should be called DiagnosticCLIToolConfig?
+class CLIToolConfig(BaseModel):
+    """Configuration options for the server."""
+
+    language_id: str = Field(None)
+    """
+    The language to which will trigger this CLI tool behaviour.
+    Must be a `language_id` recognised by LSP, eg; `json`, `python`, etc
+    """
+
+    command: List[str] = Field(None)
+    """
+    The command to run, eg; `["jsonlint", "--strict"]`
+    """
+
+    parsing: OutputParsingConfig = Field(None)
+    """Config for the output of CLI tools"""
+
+
+class InitializationOptions(BaseModel):
+    """The initialization options we can expect to receive from a client."""
+
+    clitool_configs: List[CLIToolConfig] = Field()
+    """
+    Parent field for all CLI tool configs.
+    This is the entire collection of CLI tools, from linters, formatters, and
+    all other manner of strange and wonderful CLI tools.
+    """
--- src/lsp/definition.py
+++ src/lsp/definition.py
@@ -1,27 +1,47 @@
 import logging
 import subprocess
-from typing import List
+import typing
+from typing import Any, Dict, List, Optional
 
-# type: ignore
-from parse import parse
-from pygls.lsp.methods import TEXT_DOCUMENT_DID_CHANGE, TEXT_DOCUMENT_DID_OPEN
+from parse import parse  # type: ignore
+from pygls.lsp.methods import (INITIALIZE, TEXT_DOCUMENT_DID_CHANGE,
+                               TEXT_DOCUMENT_DID_OPEN)
 from pygls.lsp.types import (Diagnostic, DidChangeTextDocumentParams,
-                             DidOpenTextDocumentParams, Position, Range)
+                             DidOpenTextDocumentParams, InitializeParams,
+                             Position, Range)
 from pygls.server import LanguageServer
+
+from .clitool_config import InitializationOptions
 
 
 class CLIToolsLanguageServer(LanguageServer):
     def __init__(self):
         super().__init__()
 
+        self.user_config: Optional[InitializationOptions] = None
+        """The user's configuration."""
+
+    def initialize(self, params: InitializeParams):
+        self.user_config = InitializationOptions(
+            **typing.cast(Dict, params.initialization_options)
+        )
+
+    @property
+    def configuration(self) -> Dict[str, Any]:
+        """Return the server's actual configuration."""
+        if not self.user_config:
+            return {}
+
+        # TODO: research if there is an equivalent .dataclass() converter
+        return self.user_config.dict()
+
 
 server = CLIToolsLanguageServer()
 
-config = {
-    "command": ["jq", "."],
-    "efm": "{msg} at line {line:d}, column {col:d}",
-    "language_id": "json",
-}
+
+@server.feature(INITIALIZE)
+def on_initialize(params: InitializeParams):
+    server.initialize(params)
 
 
 @server.feature(TEXT_DOCUMENT_DID_CHANGE)
@@ -38,8 +58,44 @@ async def did_open(params: DidOpenTextDocumentParams):
     )
 
 
+def parse_line(
+    error_format: str, line_offset: int, col_offset: int, line: str
+) -> Diagnostic:
+    parsed = parse(error_format, line)
+    if parsed is None:
+        # TODO: What's the proper way of communicating this?
+        diagnostic = Diagnostic(
+            range=Range(
+                start=Position(line=0, character=0),
+                end=Position(line=0, character=1),
+            ),
+            message="CLI Tool LSP failed to parse CLI output",
+            source=type(server).__name__,
+        )
+    else:
+        line_number = parsed["line"]
+        col_number = parsed["col"]
+        message = parsed["msg"]
+
+        line_number += line_offset
+        col_number += col_offset
+
+        diagnostic = Diagnostic(
+            range=Range(
+                start=Position(line=line_number, character=col_number),
+                end=Position(line=line_number, character=col_number),
+            ),
+            message=message,
+            source=type(server).__name__,
+        )
+    return diagnostic
+
+
 def diagnose(uri: str):
     document = server.workspace.get_document(uri)
+
+    # TODO: onlty run the CLI tools that match the current language ID
+    config = server.configuration["clitool_configs"][0]
 
     if document.language_id != config["language_id"]:
         return
@@ -56,33 +112,19 @@ def diagnose(uri: str):
     diagnostics: List[Diagnostic] = []
 
     if not output:
+        # Clear diagnostics
         server.publish_diagnostics(document.uri, diagnostics)
         return
 
-    parsed = parse(config["efm"], output)
-    if parsed is None:
-        # TODO: What's the proper way of communicating this?
-        diagnostic = Diagnostic(
-            range=Range(
-                start=Position(line=0, character=0),
-                end=Position(line=0, character=1),
-            ),
-            message="CLI Tool LSP failed to parse CLI output",
-            source=type(server).__name__,
-        )
-    else:
-        line = parsed["line"]
-        col = parsed["col"]
-        message = parsed["msg"]
+    logging.error(msg=config["parsing"]["format"])
 
-        diagnostic = Diagnostic(
-            range=Range(
-                start=Position(line=line, character=col),
-                end=Position(line=line, character=col),
-            ),
-            message=message,
-            source=type(server).__name__,
+    for line in output.splitlines():
+        diagnostic = parse_line(
+            config["parsing"]["format"],
+            config["parsing"]["line_offset"],
+            config["parsing"]["col_offset"],
+            line,
         )
-    diagnostics.append(diagnostic)
+        diagnostics.append(diagnostic)
 
     server.publish_diagnostics(document.uri, diagnostics)
